de90c497e4f4ee8e9d67b79926a81bc2630aaf05
Masterxxmits <idrismitsaal@gmail.com>
2024-11-26 12:20:41 -0600

    GAMBA

---
+++ 28 Capstone Coding/sketch.js
@@ -0,0 +1,76 @@
+// GAMBA Casino
+// Mitsaal Idris
+// TBD
+
+let sequence = [];
+let stepAmount = 1;
+let currentValue = 0;
+let largest = 0;
+let scaleAmount = 0;
+let arcList = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  stroke(255);
+  noFill();
+}
+
+function draw() {
+  if(largest < 1000){
+    background(151, 42, 39);
+    translate(0, height/6);
+    addToSequence();
+    scaleAmount = lerp(scaleAmount, width/largest,);
+    scale(scaleAmount);
+    renderArcs();
+  }
+  
+}
+
+function addToSequence(){
+  let backwards = currentValue - stepAmount;
+  if(backwards > 0 && !sequence.includes(backwards)){
+    arcList.push(new rArc(currentValue, backwards, sequence.length%2));
+    sequence.push(backwards);
+    currentValue = backwards;
+    stepAmount++;
+  }
+  else{
+    let forwards = currentValue + stepAmount;
+    arcList.push(new rArc(currentValue, forwards, sequence.length%2));
+    sequence.push(forwards);
+    currentValue = forwards;
+    stepAmount++;
+    if(currentValue > largest){
+      largest = currentValue;
+    }
+    
+  }
+  
+}
+
+function renderArcs(){
+  for(let r of arcList){
+    r.display();
+  }
+}
+
+
+class rArc{
+  constructor(start,end, direction){
+    this.start = start;
+    this.end = end;
+    this.direction = direction;
+  }
+  display(){
+    let diameter = abs(this.start - this.end);
+    let x = (this.start + this.end)/2; //centerpoint
+    strokeWeight(0.5);
+    if(this.direction === 0){ //forward
+      arc(x, 0, diameter, diameter, 0, PI);
+    }
+    else{
+      arc(x, 0, diameter, diameter, PI, 0);
+    }
+  }
+}
