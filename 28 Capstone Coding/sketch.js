ac8ff0dce0aef77e341b0b4f10d4cf5cbd6b364b
Masterxxmits <idrismitsaal@gmail.com>
2024-12-02 12:34:39 -0600

    GAMBLE CASINO
    made the logo move up and down got a blackjack logo sprite

--- 28 Capstone Coding/sketch.js
+++ 28 Capstone Coding/sketch.js
@@ -3,24 +3,36 @@
 // TBD
 
 let logo; 
+let bJLogo;
+let logoY = 0;
 
-// function preload(){
-//   logo = loadImage('assets/LOGO.png');
-  
-// }
+function preload(){
+  logo = loadImage('assets/GAMBA LOGO (1).png');
+  bJLogo = loadImage('assets/BLACKJACK LOGO.png');
+}
 
 function setup() {
+  logoY = height/15;
   createCanvas(windowWidth, windowHeight);
   stroke(255);
   noFill();
-  // image(logo, 0, 0);
-  // logo.hide();
+  
+  // imageMode(CENTER);
+}
+
+function redBackGround(){
+  background(220+sin(frameCount/20)*30, 0,0);
+  stroke(100, 190);
+  for(let x = -width; x <width; x += 20){
+    line(x, 0, x+800, height);
+  }
 }
 
 function draw() {
-  // let ctx = (a, canvas, context);
-  // ctx.canvas.width  = window.innerWidth;
-  // ctx.canvas.height = window.innerHeight;
+  // background(255, 50, 5);
+  redBackGround();
+  image(logo, width/2.4, logoY+sin(frameCount/20)* 30, 400, 330);
+  image(bJLogo, width/10, height/3, 400, 300);
   
 }
 
