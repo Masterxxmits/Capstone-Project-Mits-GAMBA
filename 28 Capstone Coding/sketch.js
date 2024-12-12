6b32fb027cfa543e64386f483c61e774f6c45c1d
Masterxxmits <idrismitsaal@gmail.com>
2024-12-12 12:32:57 -0600

    GAMBA CASINO
    BASE SLOT MACHINE WORK, START ON ROULETTE

--- 28 Capstone Coding/sketch.js
+++ 28 Capstone Coding/sketch.js
@@ -2,7 +2,7 @@
 // Mitsaal Idris
 // TBD
 
-let logo; 
+let logo;
 let bJLogo;
 let rLogo;
 let sLogo;
@@ -24,7 +24,7 @@ let blackJackA = 0;
 let rouletteA = 0;
 let topLayer;
 
-function preload(){
+function preload() {
   logo = loadImage('assets/GAMBA LOGO (1).png');
   bJLogo = loadImage('assets/BLACKJACK LOGO.png');
   rLogo = loadImage('assets/ROULETTE LOGO.png');
@@ -38,174 +38,272 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  topLayer = createGraphics(800,750);
+  topLayer = createGraphics(800, 750);
   imageMode(CENTER);
-  logoY = height/5.5;
-  bJY = height/2.1;
-  rY = height/1.4;
-  sY = height/2.3;
+  logoY = height / 5.5;
+  bJY = height / 2.1;
+  rY = height / 1.4;
+  sY = height / 2.3;
   mySlotGame = new SlotGame();
   stroke(255);
   noFill();
   // imageMode(CENTER);
 }
 
-function redBackGround(){
-  background(220+sin(frameCount/20)*30, 0,0);
+function redBackGround() {
+  background(220 + sin(frameCount / 20) * 30, 0, 0);
   stroke(100, 190);
-  for(let x = -width; x <width; x += 20){
-    line(x, 0, x+800, height);
+  for (let x = -width; x < width; x += 20) {
+    line(x, 0, x + 800, height);
   }
 }
 
 function draw() {
   // background(255, 50, 5);
-  if((mouseX > width/5-115 && mouseX < width/5) && (mouseY > bJY-125 && mouseY < bJY+55)){
+  if ((mouseX > width / 5 - 115 && mouseX < width / 5) && (mouseY > bJY - 125 && mouseY < bJY + 55)) {
     bJHover = true;
-    bJY = max(bJY - 3, height/2.1 - 50);
+    bJY = max(bJY - 3, height / 2.1 - 50);
   }
-  else{
+  else {
     bJHover = false;
-    bJY = min(bJY + 3, height/2.1);
+    bJY = min(bJY + 3, height / 2.1);
   }
-  if((mouseX > width*0.45 && mouseX < width*0.55) && (mouseY > rY-125 && mouseY < rY+55)){
+  if ((mouseX > width * 0.45 && mouseX < width * 0.55) && (mouseY > rY - 125 && mouseY < rY + 55)) {
     rHover = true;
-    rY = max(rY - 3, height/1.4 - 50);
+    rY = max(rY - 3, height / 1.4 - 50);
   }
-  else{
+  else {
     rHover = false;
-    rY = min(rY + 3, height/1.4);
+    rY = min(rY + 3, height / 1.4);
   }
-  if((mouseX > width*0.76 && mouseX < width*0.91) && (mouseY > sY-125 && mouseY < sY+55)){
+  if ((mouseX > width * 0.76 && mouseX < width * 0.91) && (mouseY > sY - 125 && mouseY < sY + 55)) {
     sHover = true;
-    sY = max(sY - 3, height/2.3 - 50);
+    sY = max(sY - 3, height / 2.3 - 50);
   }
-  else{
+  else {
     sHover = false;
-    sY = min(sY + 3, height/2.3);
+    sY = min(sY + 3, height / 2.3);
   }
   redBackGround();
-  image(logo, width/1.99, logoY+sin(frameCount/20)* 30, 400, 330);
-  image(bJLogo, width/6, bJY, 400, 300);
-  image(rLogo, width/2, rY, 370, 300);
-  image(sLogo, width/1.2, sY, 400, 300);
+  image(logo, width / 1.99, logoY + sin(frameCount / 20) * 30, 400, 330);
+  image(bJLogo, width / 6, bJY, 400, 300);
+  image(rLogo, width / 2, rY, 370, 300);
+  image(sLogo, width / 1.2, sY, 400, 300);
   // fill(0)
   // rect(width*0.45, rY-125, width*0.1, 180)
-  if(slotsA === 1){
-    background(237+sin(frameCount/20)*30, 205+sin(frameCount/20)*30,98+sin(frameCount/20)*30);
-    for(let x = -width; x <width; x += 20){
-      line(x, 0, x+800, height);
+  if (slotsA === 1) {
+    background(237 + sin(frameCount / 20) * 30, 205 + sin(frameCount / 20) * 30, 98 + sin(frameCount / 20) * 30);
+    for (let x = -width; x < width; x += 20) {
+      line(x, 0, x + 800, height);
     }
-    
+
     fill(255);
     stroke(0);
     rectMode(CENTER);
-    rect(width/2, height/2, 250, 750);
-    rect(width/2.8, height/2, 250, 750);
-    rect(width/1.55, height/2, 250, 750);
+    rect(width / 2, height / 2, 250, 750);
+    rect(width / 2.8, height / 2, 250, 750);
+    rect(width / 1.55, height / 2, 250, 750);
     topLayer.clear();
     mySlotGame.display();
-    image(topLayer, width/2, height/2, 800, 750);
+    image(topLayer, width / 2, height / 2, 800, 750);
   }
-  if(blackJackA === 1){
-    background(53+sin(frameCount/20)*30, 101+sin(frameCount/20)*30,77+sin(frameCount/20)*30);
-    for(let x = -width; x <width; x += 20){
-      line(x, 0, x+800, height);
+  if (blackJackA === 1) {
+    background(53 + sin(frameCount / 20) * 30, 101 + sin(frameCount / 20) * 30, 77 + sin(frameCount / 20) * 30);
+    for (let x = -width; x < width; x += 20) {
+      line(x, 0, x + 800, height);
     }
   }
-  if(rouletteA === 1){
-    background(145+sin(frameCount/20)*30, 193+sin(frameCount/20)*30,214+sin(frameCount/20)*30);
-    for(let x = -width; x <width; x += 20){
-      line(x, 0, x+800, height);
+  if (rouletteA === 1) {
+    background(145 + sin(frameCount / 20) * 30, 193 + sin(frameCount / 20) * 30, 214 + sin(frameCount / 20) * 30);
+    for (let x = -width; x < width; x += 20) {
+      line(x, 0, x + 800, height);
     }
+    angleMode(DEGREES);
+    fill(255);
+    stroke(0);
+    rTable();
+    fill(25,25,100);
+    circle(0,0,550);
+    // noFill();
+    // circle(0,0, 600);
+  }
+}
+
+function keyPressed() {
+  if (keyIsDown(32)) {
+    mySlotGame.stopLane();
   }
 }
 
 function mousePressed() {
   // click detection
-  if((mouseX > width/5-115 && mouseX < width/5) && (mouseY > bJY-125 && mouseY < bJY+55)){
-    if(mouseIsPressed){
+  if ((mouseX > width / 5 - 115 && mouseX < width / 5) && (mouseY > bJY - 125 && mouseY < bJY + 55)) {
+    if (mouseIsPressed) {
       blackJackA = 1;
     }
   }
-  if((mouseX > width*0.45 && mouseX < width*0.55) && (mouseY > rY-125 && mouseY < rY+55)){
+  if ((mouseX > width * 0.45 && mouseX < width * 0.55) && (mouseY > rY - 125 && mouseY < rY + 55)) {
     console.log(mouseX, mouseY);
-    if(mouseIsPressed){
+    if (mouseIsPressed) {
       rouletteA = 1;
     }
   }
-  if((mouseX > width*0.76 && mouseX < width*0.91) && (mouseY > sY-125 && mouseY < sY+55)){
-    if(mouseIsPressed){
+  if ((mouseX > width * 0.76 && mouseX < width * 0.91) && (mouseY > sY - 125 && mouseY < sY + 55)) {
+    if (mouseIsPressed) {
       slotsA = 1;
     }
   }
 }
+function rTable() {
+  push();
+  translate(400,500);
+  circle(0,0,750);
+  let red = true;
+  for (let a = 0; a <=360; a+= 360/38){
+    if(red){
+      fill(255,0,0);
+    }
+    else{
+      fill(0);
+    }
+    red = !red;
+    arc(0,0,750,750,(a),(a+360/38));
+  }
+  fill(0,255,0);
+  arc(0,0,750,750,(0),(360/38));
+  fill(0,255,0);
+  arc(0,0,750,750,(180),(180+360/38));
+  
+  //rotate(frameCount);
+//   let minArm = map(1,0,width,0,360);
+//   minArm = floor(minArm);
+//   for(let i = 0; i < 12; i++){
+//     line(0,0,0,0);
+//     rotate(360/12);
+//   }
+//   for(let i = 0; i < 38; i++){
+//     line(0,0,375,0);
+//     rotate(360/38);
+//   }
+//   rotate(frameCount/10);
+//   let HrArm = map(1,0,width,0,360);
+//   HrArm = floor(HrArm);
+//   for(let i = 0; i < 1; i++){
+//     line(0,0,0,0);
+//   }
+//   rotate(frameCount);
+//   let slowArm = map(1,0,width,0,360);
+//   slowArm = floor(slowArm);
+//   for(let i = 0; i < 1; i++){
+//     line(0,0,0,0);
+//   }
+ }
 
-class SlotGame{
-  constructor(){
+class SlotGame {
+  constructor() {
+    this.value = 0;
     this.col1 = [];
     this.col2 = [];
     this.col3 = [];
-    this.col1.push(new SlotTile(0,-500,"7"));
-    this.col1.push(new SlotTile(0,-250,"Cherry"));
-    this.col1.push(new SlotTile(0,0,"Lemon"));
-    this.col1.push(new SlotTile(0,250,"Bar"));
-    this.col1.push(new SlotTile(0,500,"Orange"));
-    this.col2.push(new SlotTile(275,-500,"Cherry"));
-    this.col2.push(new SlotTile(275,-250,"Lemon"));
-    this.col2.push(new SlotTile(275,0,"Bar"));
-    this.col2.push(new SlotTile(275,250,"Orange"));
-    this.col2.push(new SlotTile(275,500,"7"));
-    this.col3.push(new SlotTile(555,-500,"Lemon"));
-    this.col3.push(new SlotTile(555,-250,"Bar"));
-    this.col3.push(new SlotTile(555,0,"Orange"));
-    this.col3.push(new SlotTile(555,250,"7"));
-    this.col3.push(new SlotTile(555,500,"Cherry"));
+    this.col1.push(new SlotTile(0, -500, "7"));
+    this.col1.push(new SlotTile(0, -250, "Cherry"));
+    this.col1.push(new SlotTile(0, 0, "Lemon"));
+    this.col1.push(new SlotTile(0, 250, "Bar"));
+    this.col1.push(new SlotTile(0, 500, "Orange"));
+    this.col2.push(new SlotTile(275, -500, "Lemon"));
+    this.col2.push(new SlotTile(275, -250, "7"));
+    this.col2.push(new SlotTile(275, 0, "Orange"));
+    this.col2.push(new SlotTile(275, 250, "Bar"));
+    this.col2.push(new SlotTile(275, 500, "Cherry"));
+    this.col3.push(new SlotTile(555, -500, "Orange"));
+    this.col3.push(new SlotTile(555, -250, "7"));
+    this.col3.push(new SlotTile(555, 0, "Cherry"));
+    this.col3.push(new SlotTile(555, 250, "Bar"));
+    this.col3.push(new SlotTile(555, 500, "Lemon"));
   }
-  display(){
-    for(let s of this.col1){
-      s.display();
-      s.update();
+
+  stopLane() {
+    this.value += 1;
+    if(this.value === 1){
+      print("snap1");
+      for (let s of this.col1) {
+        s.gridSnap();
+      }
     }
-    for( let s of this.col2){
-      s.display();
-      s.update();
+    if(this.value === 2){
+      print("snap2");
+      for (let s of this.col2) {
+        s.gridSnap();
+      }
     }
-    for(let s of this.col3){
+    if(this.value === 3){
+      print("snaprhombus");
+      for (let s of this.col3) {
+        s.gridSnap();
+      }
+    }
+  }
+  display() {
+
+    for (let s of this.col1) {
+      if(this.value === 0){
+        s.update();
+      }
       s.display();
-      s.update();
+    }
+    for (let s of this.col2) {
+      if (this.value < 2) {
+        s.update();
+      }
+      s.display();
+    }
+    for (let s of this.col3) {
+      if ( this.value < 3) {
+        s.update();
+      }
+      s.display();
     }
   }
 }
 
-class SlotTile{
-  constructor(x, y, value){
+class SlotTile {
+  constructor(x, y, value) {
     this.x = x;
     this.y = y;
     this.value = value;
-    this.speed = 66;
+    this.speed = 69;
   }
-  display(){
-    if (this.value === "7"){
-      topLayer.image(slotSeven,this.x, this.y);
+  display() {
+    if (this.value === "7") {
+      topLayer.image(slotSeven, this.x, this.y);
     }
-    if (this.value === "Bar"){
+    if (this.value === "Bar") {
       topLayer.image(slotBar, this.x, this.y);
     }
-    if (this.value === "Cherry"){
+    if (this.value === "Cherry") {
       topLayer.image(slotCherry, this.x, this.y);
     }
-    if (this.value === "Lemon"){
+    if (this.value === "Lemon") {
       topLayer.image(slotLemon, this.x, this.y);
     }
-    if (this.value === "Orange"){
+    if (this.value === "Orange") {
       topLayer.image(slotOrange, this.x, this.y);
     }
   }
-  update(){
+  update() {
     this.y += this.speed;
-    if(this.y >= 750){
+    if (this.y >= 750) {
       this.y -= 1250;
     }
   }
+  gridSnap(){
+    while (this.y%250!==0){
+      this.y++;
+    }
+    if (this.y >= 750) {
+      this.y -= 1250;
+    }
+
+  }
+
 }
